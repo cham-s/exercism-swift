51e3685a3294e11fc0182b311223fb8d9183fdb0
cham-s <cattouma@student.42.fr>
2017-10-03 21:24:53 +0200

    fixed when input has no letter

--- bob/Sources/Bob.swift
+++ bob/Sources/Bob.swift
@@ -1,5 +1,7 @@
 //Solution goes in Sources
 
+import Foundation
+
 extension String {
     func isEmptyOfWord() -> Bool {
         return isEmpty || (filter { return $0 != " " }).isEmpty
@@ -12,6 +14,19 @@ extension String {
     func isAllUpperCased() -> Bool {
         return self.uppercased() == self
     }
+    
+    func hasLetter() -> Bool {
+        let pattern = "\\p{letter}"
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        let matches = regex.matches(in: self, options: [], range: NSRange(location: 0, length: self.count))
+        return matches.isEmpty.NOT
+    }
+}
+
+extension Bool {
+    var NOT: Bool {
+        return !self
+    }
 }
 
 struct Bob {
@@ -19,7 +34,7 @@ struct Bob {
         switch input {
         case let string where string.isEmptyOfWord():
             return "Fine. Be that way!"
-        case let string where string.isAllUpperCased():
+        case let string where string.isAllUpperCased() && string.hasLetter():
             return "Whoa, chill out!"
         case let string where string.hasQuestionMarkAtTheEnd():
             return "Sure."
