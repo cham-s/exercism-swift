59dfe25bb33574b606116d65d9d582fc7839c341
cham-s <cattouma@student.42.fr>
2017-10-05 21:21:38 +0200

    submitted version hamming added

--- hamming/Sources/Hamming.swift
+++ hamming/Sources/Hamming.swift
@@ -1,1 +1,21 @@
 //Solution goes in Sources
+
+import Foundation
+
+
+struct Hamming {
+    static func compute(_ with: String, against: String) -> Int? {
+        guard with.count == against.count else {
+            return nil
+        }
+        
+        let withArray = Array(with)
+        let angainstArray = Array(against)
+        var hammingDistance = 0
+        for i in 0..<with.count {
+            hammingDistance += withArray[i] == angainstArray[i] ? 0 : 1
+        }
+        return hammingDistance
+    }
+}
+
--- hamming/Tests/HammingTests/HammingTests.swift
+++ hamming/Tests/HammingTests/HammingTests.swift
@@ -54,3 +54,4 @@ class HammingTests: XCTestCase {
         ]
     }
 }
+
