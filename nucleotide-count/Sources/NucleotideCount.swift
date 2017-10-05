98f78d48c6c054a1586d7d98df9c696567cad274
cham-s <cattouma@student.42.fr>
2017-10-05 22:18:55 +0200

    submitted version of nuc-count

--- nucleotide-count/Sources/NucleotideCount.swift
+++ nucleotide-count/Sources/NucleotideCount.swift
@@ -1,1 +1,30 @@
 //Solution goes in Sources
+
+extension Bool {
+    var NOT: Bool {
+        return !self
+    }
+}
+
+struct DNA {
+    let strand: String
+    private let validNucleotides = "ATCG"
+    
+    init?(strand: String) {
+        for character in strand {
+            if validNucleotides.contains(character).NOT { return nil }
+        }
+        self.strand = strand
+    }
+    
+    func count(_ nucleotide: Character) -> Int {
+        let nucleotides = strand.filter { $0 == nucleotide }
+        return nucleotides.count
+    }
+    
+    func counts() -> [String: Int] {
+        var strandsCount: [String: Int] = [ "A": 0, "T": 0, "C": 0, "G": 0 ]
+        for character in strand { strandsCount[String(character)]! += 1 }
+        return strandsCount
+    }
+}
