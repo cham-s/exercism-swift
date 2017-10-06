f2ed9ee809086b41dc62a75371064a301329c878
cham-s <cattouma@student.42.fr>
2017-10-06 20:30:20 +0200

    added submitted version

--- accumulate/Sources/Accumulate.swift
+++ accumulate/Sources/Accumulate.swift
@@ -1,1 +1,11 @@
 //Solution goes in Sources
+
+extension Array {
+    func accumulate(_ accumulator: (Element) -> Element) -> [Element] {
+        var newArray = [Element]()
+        for element in self {
+            newArray.append(accumulator(element))
+        }
+        return newArray
+    }
+}
