3e35c1ea63ebcdfef703801a878d0e7ad365ed32
cham-s <cattouma@student.42.fr>
2017-10-08 07:06:16 +0200

    added submitted version

--- isogram/Sources/Isogram.swift
+++ isogram/Sources/Isogram.swift
@@ -1,1 +1,22 @@
 //Solution goes in Sources
+
+import Foundation
+
+extension String {
+    var isLetter: Bool {
+        return (self.rangeOfCharacter(from: .letters)) != nil
+    }
+}
+
+struct Isogram {
+    static func isIsogram(_ string: String) -> Bool {
+        var visited = ""
+        for char in string.lowercased() {
+            let currentCharAsString = String(char)
+            guard currentCharAsString.isLetter else { continue }
+            if visited.contains(char) { return false }
+            visited += currentCharAsString
+        }
+        return true
+    }
+}
