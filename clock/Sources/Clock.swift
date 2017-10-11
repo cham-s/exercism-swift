005ed11fc16fdfa5a74b7d9a12de25f2eae0db5b
cham-s <cattouma@student.42.fr>
2017-10-11 21:06:59 +0200

    first draft

--- clock/Sources/Clock.swift
+++ clock/Sources/Clock.swift
@@ -1,1 +1,21 @@
 //Solution goes in Sources
+
+struct Clock {
+    typealias Minutes = Int
+    typealias Hours = Int
+    let minutesInAnHour: Minutes = 60
+    var hours: Hours
+    var minutes: Minutes
+    
+    init(hours: Hours = 0, minutes: Minutes = 0) {
+        self.hours = hours
+        self.minutes = minutes
+    }
+    
+    var description: String {
+        let totalMinutes: Minutes = (hours * minutesInAnHour) + minutes
+        let finalHours: Hours = totalMinutes / minutesInAnHour
+        let finalMinutes: Minutes = totalMinutes % minutesInAnHour
+        return String(format: "%d:%d", finalHours, finalMinutes)
+    }
+}
