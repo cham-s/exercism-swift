5cf5d5fa336d5476229d71bc0473207e4d11d064
cham-s <cc.attou@gmail.com>
2019-01-20 21:56:41 +0100

    update twofer

--- two-fer/Sources/TwoFer/TwoFer.swift
+++ two-fer/Sources/TwoFer/TwoFer.swift
@@ -1,10 +1,7 @@
 //Solution goes in Sources
 
 struct TwoFer {
-    static func twoFer(name: String = "") -> String {
-        guard name.isEmpty else {
-            return "One for \(name), one for me."
-        }
-        return "One for you, one for me."
+    static func twoFer(name: String = "you") -> String {
+        return "One for \(name), one for me."
     }
 }
