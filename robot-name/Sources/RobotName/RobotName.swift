814df235a9de71ccd4fdf3e99573aac1ab99ff1b
cham-s <cc.attou@gmail.com>
2019-01-29 23:12:39 +0100

    added scrable and robot

--- robot-name/Sources/RobotName/RobotName.swift
+++ robot-name/Sources/RobotName/RobotName.swift
@@ -1,1 +1,41 @@
 //Solution goes in Sources
+import Foundation
+
+struct Robot {
+    var name = ""
+    private static var names: Set<String> = Set<String>()
+    
+    init() {
+        name = generateName()
+        Robot.names.insert(name)
+    }
+    
+    mutating func resetName() {
+        Robot.names.remove(name)
+        name = generateName()
+    }
+    
+    private func generateName() ->  String {
+        let upperAlpha = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        let numbers = "0123456789"
+        var name = String(upperAlpha.randomFor(2)) + String(numbers.randomFor(3))
+        while Robot.names.contains(name) {
+            name =  String(upperAlpha.randomFor(2)) + String(numbers.randomFor(3))
+        }
+        Robot.names.insert(name)
+        return name
+    }
+}
+
+extension Collection {
+    func randomFor(_ amount: Int) -> [Element] {
+        guard isEmpty == false else { return [] }
+        var elements: [Element] = []
+        elements.reserveCapacity(amount)
+        for _ in 0..<amount {
+            elements.append(randomElement()!)
+        }
+        return elements
+    }
+}
+
