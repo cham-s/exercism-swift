87ffaffe714664557e23174a7fdac65cc911083e
cham-s <cc.attou@gmail.com>
2019-02-08 03:51:48 +0100

    first ver of secret-handshake

--- secret-handshake/Sources/SecretHandshake/SecretHandshake.swift
+++ secret-handshake/Sources/SecretHandshake/SecretHandshake.swift
@@ -1,1 +1,21 @@
-//Solution goes in Sources
+func &<T: UnsignedInteger>(left: T, right: T) ->  Bool {
+    return left & right != 0
+}
+
+struct SecretHandshake {
+    private let secrets: [(key: UInt16, value: String)] = [
+        (key: 1, value: "wink"),
+        (key: 2, value: "double blink"),
+        (key: 4, value: "close your eyes"),
+        (key: 8, value: "jump")
+    ]
+    private let reversable: UInt16 = 16
+    
+    var commands: [String] = []
+    
+    init(_ input: UInt16) {
+        commands = secrets.filter { input & $0.key }.map { $0.value }
+        if input & reversable { commands.reverse() }
+    }
+}
+
