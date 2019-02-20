13b460c4f332d2b1b1f9a12b3aab60ecffeb362b
cham-s <cc.attou@gmail.com>
2019-02-20 16:20:28 +0100

    using rows[col]

--- matrix/Sources/Matrix/Matrix.swift
+++ matrix/Sources/Matrix/Matrix.swift
@@ -1,6 +1,7 @@
 struct Matrix {
     var strMatrix: String
     var rows: [[Int]] {
+        guard strMatrix.isEmpty == false else { return [] }
         return strMatrix.split(separator: "\n")
             .map { $0.split(separator: " ")
                 .map { Int(String($0))! } }
@@ -8,13 +9,13 @@ struct Matrix {
     
     var columns: [[Int]] {
         var array: [[Int]] = []
+        guard rows.isEmpty == false else { return [] }
         let count = rows.first!.count
         
         for col in 0..<count {
             var numbers: [Int] = []
             for row in rows {
-                let number = Int(String(Array(row)[col]))!
-                numbers.append(number)
+                numbers.append(row[col])
             }
             array.append(numbers)
         }
