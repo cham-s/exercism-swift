0ec249a9e5c20397e68aed4b9ab1e3a8429b5477
cham-s <cc.attou@gmail.com>
2019-02-22 20:04:40 +0100

    first version of largest-series

---
+++ largest-series-product/Sources/LargestSeriesProduct/LargestSeriesProduct.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+struct NumberSeries {
+    enum NumberSeriesError: Error {
+        case negativeSpan
+        case invalidCharacter
+        case spanLongerThanStringLength
+    }
+    
+    var number: String
+    
+    init(_ number: String) {
+        self.number = number
+    }
+    
+    public func largestProduct(_ span: Int) throws -> Int {
+        guard span > 0 else {
+            if span == 0 { return 1 }
+            throw NumberSeriesError.negativeSpan
+        }
+        guard span <= number.count else {
+            throw NumberSeriesError.spanLongerThanStringLength
+        }
+        guard (number.rangeOfCharacter(from:
+            CharacterSet.decimalDigits.inverted)) == nil else {
+                throw NumberSeriesError.invalidCharacter
+        }
+        
+        var largest = 0
+        let limit = number.count - span
+        
+        for startOffset in stride(from: 0, through: limit, by: 1) {
+            let startIndex = number.index(number.startIndex, offsetBy: startOffset)
+            let endIndex = number.index(startIndex, offsetBy: span)
+            let slice = number[startIndex..<endIndex]
+            print(slice)
+            let product = number[startIndex..<endIndex]
+                .map { Int(String($0))! }.reduce(1, *)
+            largest = product > largest ? product : largest
+        }
+        return largest
+    }
+}
+
